4feddf71d805cccbf9cb9b6c61d6271bfd7fd5d7
MikrosDi <dimon96mik@mail.ru>
2021-11-14 21:48:41 +0300

    Добавьте файлы проекта.

---
+++ DZ_OOP_8/DZ_OOP_8.cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+#include "Dz8.h"
+
+using namespace std;
+
+
+
+int main()
+{
+	//Task 1
+
+	{
+
+		cout << "Task 1:\n";
+		int numerator = 10;
+		double denominator = 18;
+		try
+		{
+			if (denominator == 0) throw overflow_error("Division by zero");
+			cout << numerator << " / " << denominator << " = " << div(numerator, denominator);
+		}
+		catch (overflow_error)
+		{
+			cout << "Division by zero!";
+		}
+	}
+
+	// Task 2
+
+	{
+
+		cout << "\n\nTask 2:\n";
+		Bar* bar = new Bar();
+		int n = 0;
+		try
+		{
+			bool nNotZero = true;
+			while (nNotZero)
+			{
+				cout << "Enter n: ";
+				cin >> n;
+				if (n != 0)
+				{
+					bar->set(n);
+				}
+				else nNotZero = false;
+			}
+		}
+		catch (Ex& e)
+		{
+			cout << "Exception thrown, code " << e.getX();
+		}
+		delete bar;
+	}
+
+	// Task 3
+
+	{
+
+		cout << "\nTask 3:\n";
+		int fieldSize = 10;
+		Robot* robot = new Robot(fieldSize);
+
+		while (true)
+		{
+			robot->printCoordinates();
+			robot->move();
+		}
+		delete robot;
+
+	}
+
+
+
+};
